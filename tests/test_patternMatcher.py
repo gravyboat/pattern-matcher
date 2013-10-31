542b992cf090181936ec030e23dd32adfa8b1d2d
Forrest Alvarez <forrest.alvarez@gmail.com>
2013-10-30 22:51:17 -0700

    Modified comment lines, cleaned things up a bit

--- tests/test_patternMatcher.py
+++ tests/test_patternMatcher.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python
 
+'''
+Author - Forrest Alvarez
+Date - 2013-10-30
+'''
+
 import unittest
 import sys
 
@@ -7,7 +12,9 @@ sys.path.append(os.path.join('..', 'patternMatcher'))
 
 from patternMatcher import patternMatcher
 
-# Our input lists for the matching, currently blank.
+'''
+Our input lists for the matching.
+'''
 
 simpleMatch = [3, 'x,y', '1,2,3', 'bar,foo', 
                 4, 'x/y', '1/2/3/', 'bar/baz', 'bar/foo']
@@ -17,7 +24,9 @@ weirdMatch = []
 largeMatch = []
 wildcardMatch = [2, '*,*,c', '*,b,*', 1, '/a/b/c/']
 
-# Our output lists for the matching, currently blank.
+'''
+Our output lists for the matching.
+'''
 
 simpleMatchOut = ['x,y', '1,2,3', 'NO MATCH', 'bar,foo']
 complexMatchOut = ['a,*,*', 'x,*,1', 'NO MATCH', 'NO MATCH']
@@ -33,33 +42,44 @@ class patternMatchTestCase(unittest.TestCase):
         self.weirdMatch = weirdMatch
         self.largeMatch = largeMatch
 
+    '''
+    Tests against an example that contains only a few simple items.
+    '''
 
-    # Tests against an example that contains only a few simple items.
     def test_simpleMatch(self):
         patternMatcherInstance = patternMatcher()
         matchedData = matchValues(self.simpleMatch)
         self.assertEqual(matchedData, simpleMatchOut)
 
-    # Tests against an example that has several complex matches.
+    '''
+    Tests against an example that has several complex matches.
+    '''
     def test_complexMatch(self):
         patternMatcherInstance = patternMatcher()
         matchedData = matchValues(self.complexMatch)
         self.assertEqual(matchedData, complexMatchOut)
 
-
-    # Tests against an example which contains oddball items.
+    '''
+    Tests against an example which contains oddball items.
+    NOT IMPLEMENTED
+    '''
     def test_weirdMatch(self):
         patternMatcherInstance = patternMatcher()
         matchedData = matchValues(self.weirdMatch)
         self.assertEqual(matchedData, weirdMatchOut)
 
-    # Tests against an example that's simple a lot of data to crunch.
+    '''
+    Tests against an example that's simple a lot of data to crunch.
+    NOT IMPLEMENTED
+    '''
     def test_largMatch(self):
         patternMatcherInstance = patternMatcher()
         matchedData = matchValues(self.largeMatch)
         self.assertEqual(matchedData, largeMatchOut)
 
-    # Tests against items where multiple wildcards exists, but one is better.
+    '''
+    Tests against items where multiple wildcards exists, but one is better.
+    '''
     def test_wildcardMatch(self):
         patternMatcherInstance = patternMatcher()
         matchedData = matchValues(self.wildcardMatch)
